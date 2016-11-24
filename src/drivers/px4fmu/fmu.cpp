6ce87ccea38d8675c7b5bbf03e9fd7f0facd746d
Gus Grubba <mavlink@grubba.com>
2016-11-23 22:56:05 -0500

    Handle enter binding command for the SR24

--- src/drivers/px4fmu/fmu.cpp
+++ src/drivers/px4fmu/fmu.cpp
@@ -112,6 +112,9 @@
 #if !defined(BOARD_HAS_PWM)
 #  error "board_config.h needs to define BOARD_HAS_PWM"
 #endif
+
+const char* kSystemArmedRejectBind = "[FMU] system armed, bind request rejected";
+
 class PX4FMU : public device::CDev
 {
 public:
@@ -288,6 +291,7 @@ private:
 			hrt_abstime now, bool frame_drop, bool failsafe,
 			unsigned frame_drops, int rssi);
 	void dsm_bind_ioctl(int dsmMode);
+    void sr_bind(int srModel);
 	void set_rc_scan_state(RC_SCAN _rc_scan_state);
 	void rc_io_invert();
 	void rc_io_invert(bool invert);
@@ -1309,7 +1313,11 @@ PX4FMU::cycle()
 		// Check for a DSM pairing command
 		if (((unsigned int)cmd.command == vehicle_command_s::VEHICLE_CMD_START_RX_PAIR) && ((int)cmd.param1 == 0)) {
 			dsm_bind_ioctl((int)cmd.param2);
-		}
+		} else
+        // Check for a SR/RX pairing command
+        if (((unsigned int)cmd.command == vehicle_command_s::VEHICLE_CMD_START_RX_PAIR) && ((int)cmd.param1 == 1)) {
+            sr_bind((int)cmd.param2);
+        }
 
 	}
 
@@ -2742,6 +2750,24 @@ PX4FMU::gpio_ioctl(struct file *filp, int cmd, unsigned long arg)
 }
 
 void
+PX4FMU::sr_bind(int /*srModel*/)
+{
+    if (!_armed.armed) {
+        // SR/RX unbind sequence
+        dsm_config(_rcs_fd);
+        RF_RADIO_POWER_CONTROL(false);
+        usleep(200000);
+        RF_RADIO_POWER_CONTROL(true);
+        usleep(900000);
+        char unbindcmd[] = {0x55, 0x55, 0x08, 0x04, 0x00, 0x00, 0x42, 0x49, 0x4E, 0x44, 0xB0};
+        int res = ::write(_rcs_fd, &unbindcmd[0], sizeof(unbindcmd));
+        PX4_INFO("WROTE UNBIND COMMAND: %d (fd: %d)", res, _rcs_fd);
+    } else {
+        warnx(kSystemArmedRejectBind);
+    }
+}
+
+void
 PX4FMU::dsm_bind_ioctl(int dsmMode)
 {
 	if (!_armed.armed) {
@@ -2757,7 +2783,7 @@ PX4FMU::dsm_bind_ioctl(int dsmMode)
 
 	} else {
 //        mavlink_log_info(&_mavlink_log_pub, "[FMU] system armed, bind request rejected");
-		warnx("[FMU] system armed, bind request rejected");
+		warnx(kSystemArmedRejectBind);
 	}
 }
 
